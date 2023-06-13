3aa65393aadc40e82118219a814a9ab01289401f
Lisa Walters <lisacampiwalters@gmail.com>
2023-06-12 19:06:51 -0600

    completed first project

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,13 +1,18 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            var client = new HttpClient();
-
-
+            for (var number = 0; number < 5; number++)
+            { 
+            QuoteGenerator.KanyeQwote();
+            QuoteGenerator.RonSwansonQuote();
+        }
         }//main method
         
     }//public class
-}//namespace
+}//
